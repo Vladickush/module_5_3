685eef9e7e596b8098df9ee342cc03b56830fed5
Vladickush <u.vn@mail.ru>
2024-08-29 12:30:14 +0300

    Update module_5_3.py

--- module_5_3.py
+++ module_5_3.py
@@ -8,43 +8,41 @@ class House:
         return (f'Название:{self.name}, кол-во этажей:{self.number_of_floors}')
 
     def __eq__(self, other):  # сравнение этажности обьектов
-        if isinstance(other,House):
+        if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __add__(self, value):  # добавляем этажи методом  add
-        if isinstance(value,int):
-            self.number_of_floors = self.number_of_floors + value
-        return self
-
-    def __iadd__(self, value):  # добавляем этажи методом iadd
         if isinstance(value, int):
             self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
-    def __radd__(self, value):  # добавляем этажи методом radd
-        if isinstance(value, int):
-            self.number_of_floors = value + self.number_of_floors
-        return self
+    def __iadd__(self, other: int):  # добавляем этажи методом iadd
+        return self.__add__(other)
 
-    def __gt__(self, other):  # сравнение этажей
-        if isinstance(other, House):
-            return self.number_of_floors > other.number_of_floors
+    def __radd__(self, other):  # добавляем этажи методом radd
+        return self.__add__(other)
 
-    def __ge__(self, other):  # сравнение этажей
-        if isinstance(other, House):
-            return self.number_of_floors >= other.number_of_floors
-
-    def __lt__(self, other):  # сравнение этажей
+    def __lt__(self, other):  # сравнение этажей <
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
-    def __le__(self, other):  # сравнение этажей
-        if isinstance(other, House):
-            return self.number_of_floors <= other.number_of_floors
+    def __le__(self, other):  # сравнение этажей <=
+        return self.__eq__(other) or self.__lt__(other)
 
-    def __ne__(self, other):  # сравнение этажей
-        if isinstance(other, House):
-            return self.number_of_floors != other.number_of_floors
+    def __gt__(self, other):  # сравнение этажей >
+        return not self.__le__(other)
+
+    def __ge__(self, other):  # сравнение этажей >=
+        return not self.__lt__(other)
+
+    def __ne__(self, other):  # сравнение этажей !=
+        return not self.__eq__(other)
 
 
 h1 = House('ЖК Эльбрус', 10)
